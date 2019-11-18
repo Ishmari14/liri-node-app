54dda0be81a5f479ec9be6e5ec32b3af2db5aa14
Jhayla Young <jhayla.young@gmail.com>
2019-11-17 20:41:44 -0600

    working on liri js

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 
 var axios = require("axios");
 
