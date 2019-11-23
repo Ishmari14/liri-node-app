86efa1e4b8cdfd81cae3e56e32764a383494775a
Jhayla Young <jhayla.young@gmail.com>
2019-11-23 11:02:32 -0600

    working on spotify

--- liri.js
+++ liri.js
@@ -8,6 +8,7 @@ var spotify = new Spotify(keys.spotify);
 
 var axios = require("axios");
 
+///read and write///
 var fs = require("fs");
 
 var moment = require("moment");
@@ -17,3 +18,39 @@ var moment = require("moment");
 var inputCommand = process.argv[2];
 
 var inputSearch = process.argv.slice(3).join(" ");
+
+///function for liri to operate///
+
+function runLiri(inputCommand, inputSearch) {
+
+    switch (inputCommand) {
+        case "concert-this":
+            bandsInTownsGet(inputSearch);
+            break;
+
+        case "spotify-this-song":
+            spotifyGet(inputSearch);
+            break;
+
+        case "movie-this":
+            ombdGet(inputSearch);
+            break;
+
+        case "do-what-it-says":
+            randomGet(inputSearch);
+            break;
+
+
+        ///if search is blank, display this///
+        default:
+            console.log("Please enter one of the following commands: 'concert-this', 'spotify-this-song', 'movie-this,'")
+
+    }
+};
+
+///Spotify API//
+
+function spotifyGet()
+
+
+runLiri(inputCommand, inputSearch);
